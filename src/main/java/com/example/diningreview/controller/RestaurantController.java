6552a8e11acf68d5368689316f0cb14403bf47fd
Issac Lopez <lopezissaca@gmail.com>
2022-07-13 13:28:48 -0600

    formattng

--- src/main/java/com/example/diningreview/controller/RestaurantController.java
+++ src/main/java/com/example/diningreview/controller/RestaurantController.java
@@ -1,41 +1,19 @@
-Skip to content
-        Search or jump to…
-        Pulls
-        Issues
-        Marketplace
-        Explore
+package com.example.diningreview.controller;
 
-@Issac-Lopez
-        brettjames250
-        /
-        dining-review
-        Public
-        Code
-        Issues
-        Pull requests
-        Actions
-        Projects
-        Wiki
-        Security
-        Insights
-        dining-review/src/main/java/com/example/diningreview/controller/RestaurantController.java /
-@brettjames250
-brettjames250 Adding restaurant search endpoint and test
-        Latest commit eb688fa on Nov 3, 2021
-        History
-        1 contributor
-        44 lines (36 sloc)  1.7 KB
+import java.util.Optional;
 
-        package com.example.diningreview.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-        import com.example.diningreview.model.Restaurant;
-        import com.example.diningreview.repositories.RestaurantRepository;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.web.bind.annotation.*;
-        import org.springframework.web.server.ResponseStatusException;
-
-        import java.util.Optional;
+import com.example.diningreview.model.Restaurant;
+import com.example.diningreview.repositories.RestaurantRepository;
 
 @RestController
 public class RestaurantController {
@@ -64,22 +42,19 @@ public class RestaurantController {
 
     @PostMapping("/restaurants")
     public Restaurant createNewRestaurant(@RequestBody Restaurant restaurant) {
-        if (restaurantRepository.existsByPostcode(restaurant.getPostcode()) && restaurantRepository.existsByName(restaurant.getName())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A restaurant with this name and postcode already exists");
+        if (restaurantRepository.existsByPostcode(restaurant.getPostcode())
+                && restaurantRepository.existsByName(restaurant.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "A restaurant with this name and postcode already exists");
         }
         return restaurantRepository.save(restaurant);
     }
-}
-© 2022 GitHub, Inc.
-        Terms
-        Privacy
-        Security
-        Status
-        Docs
-        Contact GitHub
-        Pricing
+}©2022 GitHub,Inc.Terms Privacy Security Status Docs
+
+Contact GitHub
+Pricing
         API
-        Training
-        Blog
-        About
-        Loading complete
+Training Blog
+About
+        Loading
+complete
